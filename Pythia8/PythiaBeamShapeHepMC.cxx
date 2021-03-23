3594f28b86fdee73b3866c53048ca0ad42c3d36a
bspage912 <bspage912@gmail.com>
2021-03-23 13:05:19 -0400

    Moved definition and implementation of custom beam shape class to dedicated files and implemented for PythiaBeamShapeHepMC.cxx. Modified Makefile accordingly. Also set root output file name as a user input parameter.

--- Pythia8/PythiaBeamShapeHepMC.cxx
+++ Pythia8/PythiaBeamShapeHepMC.cxx
@@ -5,6 +5,7 @@
 #include "Pythia8/BeamShape.h"
 #include "Pythia8Plugins/HepMC3.h"
 #include "HepMC3/WriterAsciiHepMC2.h"
+#include "eicBeamShape.h"
 
 
 #include<iostream>
@@ -22,121 +23,6 @@ using std::string;
 
 using namespace Pythia8;
 
-// Class to Define Beam Properties
-class MyBeamShape : public BeamShape {
-
-public:
-
-  // Constructor.
-  MyBeamShape(double ion, double lepton, double xAngle, double hadCrab, double lepCrab) 
-  {
-    mIonBeamEnergy = ion;
-    mLeptonBeamEnergy = lepton;
-    mXAngle = xAngle;
-    mHadronCrabSize = hadCrab;
-    mLeptonCrabSize = lepCrab;
-  }
-
-  // Set the two beam momentum deviations and the beam vertex.
-  virtual void pick();
-
-protected:
-
-  double mIonBeamEnergy;
-  double mLeptonBeamEnergy;
-  double mXAngle;
-  double mHadronCrabSize;
-  double mLeptonCrabSize;
-
-};
-
-void MyBeamShape::pick() {
-
-  // Reset all values.
-  deltaPxA = deltaPyA = deltaPzA = deltaPxB = deltaPyB = deltaPzB
-    = vertexX = vertexY = vertexZ = vertexT = 0.;
-
-  // Change in Beam Pz
-  double tmpPzA, tmpPzB;
-  tmpPzA = tmpPzB = 0.;
-
-  // Set beam vertex location by a two-dimensional Gaussian.
-  if(allowVertexSpread) 
-    {
-      if(sigmaVertexZ > 0.) 
-	{
-	  vertexZ = sigmaVertexZ * rndmPtr->gauss();
-	}
-    }
-
-  // Smear Beam Energy to Set Scale of Other Effects
-  if(allowMomentumSpread)
-    {
-      double gaussZA, gaussZB;
-      gaussZA = gaussZB = 0.;
-      if(sigmaPzA > 0.)
-	{
-	  gaussZA = rndmPtr->gauss();
-	  tmpPzA = mIonBeamEnergy * (sigmaPzA * gaussZA);
-	}
-      if(sigmaPzB > 0.)
-	{
-	  gaussZB = rndmPtr->gauss();
-	  tmpPzB = mLeptonBeamEnergy * (sigmaPzB * gaussZB);
-	}
-
-      // Modify Px Due to Crossing Angle
-      deltaPxA += (mIonBeamEnergy + tmpPzA)*TMath::Sin(mXAngle);
-
-      // Modify Pz Due to Crossing Angle
-      deltaPzA += (mIonBeamEnergy + tmpPzA)*TMath::Cos(mXAngle) - mIonBeamEnergy;
-      //deltaPzA += tmpPzA;
-
-      // Electron Beam Has 0 Crossing Angle
-      deltaPzB += tmpPzB;
-    }
-
-  if(allowMomentumSpread) // allowMomentumSpread
-    {
-      double gaussXA, gaussYA, gaussXB, gaussYB;
-
-      // Implement Z-Dependent Momentum Kicks from Crabbing
-      deltaPxA += mHadronCrabSize*vertexZ;
-      deltaPxB += mLeptonCrabSize*vertexZ;
-
-      if(sigmaPxA > 0.)
-	{
-	  // Ion Beam X Divergence
-	  gaussXA = rndmPtr->gauss();
-	  double div = sigmaPxA * gaussXA;
-	  double pxLocal = (mIonBeamEnergy + tmpPzA)*TMath::Sin(div); // Dispersion in Beam Frame
-	  deltaPxA += pxLocal*TMath::Cos(mXAngle); // Compensate for Crossing Angle to Lab Frame
-	}
-      if(sigmaPyA > 0.)
-	{
-	  // Ion Beam Y Divergence
-	  gaussYA = rndmPtr->gauss();
-	  double div = sigmaPyA * gaussYA;
-	  deltaPyA += (mIonBeamEnergy + tmpPzA)*TMath::Sin(div); // No Crossing Angle in Y direction
-	}
-      if(sigmaPxB > 0.)
-	{
-	  // Lepton Beam X Divergence
-	  gaussXB = rndmPtr->gauss();
-	  double div = sigmaPxB * gaussXB;
-	  deltaPxB += (mLeptonBeamEnergy + tmpPzB)*TMath::Sin(div); // Electron Beam along Z Axis
-	}
-
-      if(sigmaPyB > 0.)
-	{
-	  // Lepton Beam Y Divergence
-	  gaussYB = rndmPtr->gauss();
-	  double div = sigmaPyB * gaussYB;
-	  deltaPyB += (mLeptonBeamEnergy + tmpPzB)*TMath::Sin(div);
-	}
-    }
-}
-
 
 int main(int argc, char* argv[])
 {
@@ -153,8 +39,6 @@ int main(int argc, char* argv[])
   cout << "Steering File = " << argv[1] << endl;
   cout << "Root Output = " << rootOut << endl;
   cout << "HepMC Output = " << hepmcOut << endl;
-
-  //const char* steerFile = 
 
   // Open Root File
   TFile *ofile = TFile::Open(rootOut,"recreate");
@@ -178,7 +62,7 @@ int main(int argc, char* argv[])
   Pythia8::Event &event = p8.event;
 
   // A class to generate beam parameters according to own parametrization.
-  BeamShapePtr myBeamShape = make_shared<MyBeamShape>(275.0,18.0,0.025,0.0030,-0.0015);
+  BeamShapePtr myBeamShape = make_shared<eicBeamShape>(275.0,18.0,0.025,0.0030,-0.0015);
 
   // Hand pointer to Pythia.
   // If you comment this out you get internal Gaussian-style implementation.
